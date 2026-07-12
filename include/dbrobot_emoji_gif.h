2c3edaff45379bc3f91d325dfc34788578f2496a
Lê Điện Biên <ledienbien@gmail.com>
2026-07-12 10:40:34 +0700

    Add files via upload

--- include/dbrobot_emoji_gif.h
+++ include/dbrobot_emoji_gif.h
@@ -1,1 +1,30 @@
+/**
+ * @file dbrobot_emoji_gif.h
+ * @brief DB_robot GIF emoji component - gifs folder only
+ *
+ * This component provides 6 GIF files in the gifs/ directory.
+ * Load them via LVGL from filesystem or embed in your project.
+ */
 
+#ifndef DBROBOT_EMOJI_GIF_H
+#define DBROBOT_EMOJI_GIF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Component version string */
+const char *dbrobot_emoji_gif_get_version(void);
+
+/** Number of GIF emojis (6) */
+int dbrobot_emoji_gif_get_count(void);
+
+/** Get GIF filename by index (0..5): staticstate, sad, happy, scare, buxue,
+ * anger */
+const char *dbrobot_emoji_gif_get_name(int index);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DBROBOT_EMOJI_GIF_H */
